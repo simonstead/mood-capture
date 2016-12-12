3d68e71662e0a9f8044fd30dadc1d06f8238de2e
Simon Stead <simonstead@me.com>
2016-12-12 20:06:35 +0000

    Add bower; jquery, dyntatable. Avenir Font. updates to views and css

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use("/bower_components/", express.static(path.join(__dirname, 'bower_components')));
+
+
 app.use('/', index);
 app.use('/users', users);
 
@@ -46,17 +49,18 @@ app.use(function(err, req, res, next) {
 // pg db support
 var pg = require('pg');
 
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
+if (process.env.DATABASE_URL) {
+  pg.defaults.ssl = true;
+  pg.connect(process.env.DATABASE_URL, function(err, client) {
+    if (err) throw err;
+    console.log('Connected to postgres! Getting schemas...');
 
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
-
+    client
+      .query('SELECT table_schema,table_name FROM information_schema.tables;')
+      .on('row', function(row) {
+        console.log(JSON.stringify(row));
+      });
+  });
+}
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Add New Mood' });
 });
+
+/* GET log page. */
+router.get('/viewAll', function(req, res, next) {
+  res.render('log', { title: 'View All Moods'});
+});
+
+/* POST add new mood */
+router.POST('/addNewMood', function(req, res, next) {
+  console.log(res.body);
+  res.render('index', { title: 'New mood added' });
+})
 
 module.exports = router;
