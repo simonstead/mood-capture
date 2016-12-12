be13f7b1bd73a358cf3d441ec821224497964c47
Simon Stead <simonstead@me.com>
2016-12-12 15:53:57 +0000

    PG connection

--- app.js
+++ app.js
@@ -43,4 +43,20 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// pg db support
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
+
 module.exports = app;
