e5c1fbd64bc4fd9bca6bbf68ebbb48fd491bf47f
Simon Stead <simonstead@me.com>
2017-03-10 09:56:50 +0000

    Users can no longer sign up if that username is in use

--- models/store.js
+++ models/store.js
@@ -9,10 +9,10 @@ const bcrypt = require('bcrypt');
 
 
 const signup = (req, res) => {
-  var username = req.body.username
-  var password = req.body.password
-  var password2 = req.body.password2
-  console.log(username, password, password2);
+  const body = req.body
+  var username = body.username
+  var password = body.password
+  var password2 = body.password2
 
   if (!username || !password || !password2) {
     req.flash('error', "Please, fill in all the fields.")
@@ -32,16 +32,54 @@ const signup = (req, res) => {
     password: hashedPassword
   }
 
-  db.none("INSERT INTO users (username,password, salt) VALUES (${username}, ${password}, ${salt})", newUser)
-  .then(function(user) {
-    db.any(`select * from moods WHERE username = '${username}'`, user)
-      .then((data) => res.render('log', { title: 'View All Moods', data: data, user: user}))
-      .catch((err) => next(err))
-  }).catch(function(error) {
-    res.render('signup', {
-      error: error
-    })
-  })
+  // TODO: check for duplicate usernames!
+  //
+  // db.none("INSERT INTO users (username,password, salt) VALUES (${username}, ${password}, ${salt})", newUser)
+  // .then(function(user) {
+  //   db.any(`select * from moods WHERE username = '${username}'`, user)
+  //     .then((data) => res.render('log', { title: 'View All Moods', data: data, user: user}))
+  //     .catch((err) => next(err))
+  // }).catch(function(error) {
+  //   res.render('signup', {
+  //     error: error
+  //   })
+  // })
+
+  db.any("SELECT * FROM users WHERE username = $1", [username]).then(
+    (user) => {
+      console.log(user, user.length);
+      if (user.length !== 0) {
+        res.render('signup', {
+          validationErrors: "Sorry, that username is already taken.",
+          form: body
+        })
+      }
+      else {
+        Promise.all([
+          db.one("INSERT INTO users (username, password, salt) VALUES (${username}, ${password}, ${salt}) RETURNING *", newUser),
+          db.any(`select * from moods WHERE username = '${username}'`, user)
+        ])
+        .then(function(data) {
+          req.login(data[0], () =>
+            res.render('log', {
+                title: 'View All Moods',
+                user: data[0],
+                data: data[1],
+                confirmation: "Thank you for signing up."
+            })
+          )
+        })
+        .catch(function(error) {
+          res.render('signup', {
+            error: error,
+            form: body
+          })
+        })
+      }
+    }
+  )
+
+
 }
 
 module.exports = {
